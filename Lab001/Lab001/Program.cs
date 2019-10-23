804007f440bc74dbfcc50917262f6e34f10b157d
Rafia Sajjad <s01221065@acad.tri-c.edu>
2019-10-23 11:40:36 -0400

    Completed Lab 1

---
+++ Lab001/Lab001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Rafia Sajjad!");
+        }
+    }
+}
