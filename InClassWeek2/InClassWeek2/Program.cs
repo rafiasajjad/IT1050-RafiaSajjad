c73852f773e9e014f45c7fa6efe6ac67f1086328
Rafia Sajjad <s01221065@acad.tri-c.edu>
2019-10-30 11:59:16 -0400

    Week 2 In class discussion

---
+++ InClassWeek2/InClassWeek2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace InClassWeek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10/28/19 Monday
+
+            /*
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Appi!");
+            Console.Write("Hello Rafia!\n");
+            Console.Write("Hello\"Khoti!\n");
+            Console.Write("Hello\tPagal!\n");
+            */
+
+            // variable declaration
+            //string name;
+            //int favoriteNumber;
+
+            // assignement statements 
+            //name = "Maria";
+            //favoriteNumber = 2;
+
+            //Console.WriteLine(name + "'s favorite number is " + favoriteNumber);
+
+            //int pi = 22/7;
+            //float piFloat = 22f / 7f;
+            //double piDouble = 22d / 7d;
+            //decimal piDecimal = 22m / 7m;
+
+            //Console.WriteLine("Pi: " + pi);
+            //Console.WriteLine("Pi Float: " + piFloat);
+            //Console.WriteLine("Pi Double: " + piDouble);
+            //Console.WriteLine("Pi Decimal: " + piDecimal);
+
+            //Console.WriteLine(5 == 5);
+            //Console.WriteLine(5 == 6);
+
+            //Console.WriteLine((4 + 3) * 2);
+            //Console.WriteLine(4 + 3 * 2); 
+
+            // 10/30/19 Wednesday ******************************************
+
+            //** Different ways to write/print/define information **
+
+            //String name = "Rafia";
+            //Console.WriteLine("Hello {0}, {1} and {2} !", name, "Ifrah", "Mariam");
+
+            // ** using $ to replace/input values**
+
+            //int a = 123;
+            //int b = 234;
+            //int sum = a + b;
+            //Console.WriteLine($"Sum of {a} + {b} = {sum} ");
+
+            // ** using bool variable ** 
+
+            //bool case1 = 100 <= 100; //true//
+            //bool case2 = 10 > 11; //false// 
+
+            //Console.WriteLine(case1);
+            //Console.WriteLine(case2);
+            //Console.WriteLine(case1 && case2);
+            //Console.WriteLine(case1 || case2); 
+
+            //** using if (condition) where condition is a bool **
+
+            //int a = 4;
+            //int b = 5;
+
+            //if (a < b)
+            //{
+            //    Console.WriteLine("a is greater!");
+            //}
+
+            //if (a > b)
+            //{
+            //    Console.WriteLine("a is greater!");
+            //}
+
+            //if (a == a)
+            //{
+            //    Console.WriteLine("a is equal to a");
+            //}
+
+            //if (!(a != a))
+            //{
+            //    Console.WriteLine("a is equal to a");
+            //}
+
+
+        }
+    }
+}
