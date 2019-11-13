1fd0a3024faeb7469f2f85a9cfa70834f98fa615
Rafia Sajjad <s01221065@acad.tri-c.edu>
2019-11-13 12:04:12 -0500

    In Class Week 4

---
+++ InClassWeek4/InClassWeek4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace InClassWeek4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 1;
+            //while (i<11)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //for (int j = 1; j < 11; j++)
+            //{
+            //    Console.WriteLine(j); 
+            //}
+
+            //for (int j = 10; j > 0; j = j - 1)
+            //{
+            //    Console.WriteLine(j); 
+            //}
+
+            //Console.WriteLine("Between loops....");
+
+            //int k = 10;
+            //for (; ;)
+            //{
+            //    Console.WriteLine(k);
+            //    k = k - 1;
+            //    if (k < 1)
+            //        break;
+
+            string itemDescription;
+            double total = 0d;
+            string doContinue;
+            string display;
+            do
+            {
+                Console.Write("Enter the item (or x to end): ");
+                itemDescription = Console.ReadLine();
+
+                Console.Write("Enter the price of the item: ");
+                double itemPrice = Convert.ToDouble(Console.ReadLine());
+
+                total = total + itemPrice;
+
+                display +- itemDescription + "\t" + Math.Round(total, 2) + "\n";
+
+
+                Console.Write("Would you like to continue (y|n) ?");
+                doContinue = Console.ReadLine();
+            } while (doContinue != "n");
+
+            Console.WriteLine("Subtotal\t" + Math.Round(total,2) "\n" );
+            Console.WriteLine("Tax Amount\t" + total * 0.7);
+            Console.WriteLine("Total\t" + total * 1.07); 
+
+
+        }
+
+    }
+}
