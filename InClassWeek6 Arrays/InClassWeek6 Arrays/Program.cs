833824bb44fbe3d1190e75cde960d8afe22f7ab6
Student <Student@DESKTOP-KNDNOI3>
2019-11-25 11:56:38 -0500

    In Class Week 6

---
+++ InClassWeek6 Arrays/InClassWeek6 Arrays/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace InClassWeek6_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // *********************************************** Monday 11/25/19 ************************************************************
+
+            //string person1 = "Lopa";
+            //string person2 = "Shengyu";
+            //string person3 = "Trevor";
+            //string person4 = "Christi";
+            //string person5 = "Ryan";
+
+            string[] people = new string[5]; // length is 5 ; people is just refering to anywhere in memory 
+            people[0] = "Maria";
+            people[1] = "Naddie";
+            people[2] = "Rafia";
+            people[3] = "Christi";
+            people[4] = "Ryan";
+
+            string[] major = { "Cyber Security" ,
+                               "Computer Science" ,
+                               "Networking" ,
+                               "Software Developer" ,
+                               "Programming and Development" };
+
+            string[] number = new string[5];
+            number[0] = "2";
+            number[1] = "4";
+            number[2] = "5";
+            number[3] = "28";
+            number[4] = "55";
+
+            Console.WriteLine($"Hello, {people[2]} !");
+
+            for (int i = 0; i < people.Length; i++) // Here, length is 5 so the condition says less than 5 
+            {
+                Console.WriteLine(people[i] + ":" + major[i] + ":" + number[i]); 
+                // writing i will print all elements in array from 0 till 4 (total 5)
+            }
+
+            foreach (string person in people) // people is name of array 
+            {
+                Console.WriteLine(person);
+            }
+
+            //********************* NO CLASS WEDNESDAY 11/25/19 ****************************************************
+        } 
+    }
+}
---
+++ Reciept Example Week 6/Reciept Example Week 6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Reciept_Example_Week_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberItems;
+            Console.Write("Enter total number of items in cart: "); 
+            numberItems = Convert.ToInt32(Console.ReadLine()); 
+
+            string[] items = new string[numberItems];
+
+            double[] amounts = new double[numberItems];
+
+            for (int i = 0; i < numberItems; i++)
+            {
+                Console.Write("Enter item description: ");
+                items[i] = Console.ReadLine();
+
+                Console.Write("Enter item cost: ");
+                amounts[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            double total = 0d;
+            Console.WriteLine("**********************");
+            Console.WriteLine("Item     \tAmount");
+            Console.WriteLine("**********************");
+
+            for (int i = 0; i < amounts.Length; i++)
+            {
+                total += amounts[i];
+                Console.WriteLine($"{items[i]}       \t{amounts[i]}");
+            }
+
+            Console.WriteLine("**********************");
+
+            Console.WriteLine("Total    \t" + total);
+            Console.WriteLine("Tax Amount    \t" + (total * .07) );
+            Console.WriteLine("Total w/ tax \t" + (total * 1.07) );
+            Console.WriteLine("**********************");
+            /*
+            user input number of items in the reciept 
+            create an string array for the description of those items and an array for the amount of each item (double/decimal)
+            for loop for no. of items
+                  input amt and item desc 
+            foreach item
+                  add amt to running total 
+                  output each item 
+            output reciept total
+            */
+        }
+    }
+}
