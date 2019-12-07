cadd993e2d7274a7d992d2b25f2c1cf0b4fcbc22
Default <Default@DESKTOP-L4GGJUO>
2019-12-06 20:03:14 -0500

    Completed Lab 07

---
+++ Lab 07/Lab 07/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lab_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.Display();
+            BookTest book2 = new BookTest("Visual C#: How to Program", "Harvey Deitel, Paul Deitel", "2019");
+            book2.Display();
+        }
+    }
+    
+    class Book
+    {
+        public string title = "Visual C#: How to Program" ;
+        public string author = "Harvey Deitel, Paul Deitel";
+        public string year = "2019";
+
+        public void Display()
+        {
+            Console.WriteLine($"Course: {title}\nAuthor: {author}\nYear: {year}");
+            Console.WriteLine(); 
+        }
+    }
+    
+    class BookTest
+    {
+        public string title;
+        public string author;
+        public string year;
+        public BookTest(string title, string author, string year)
+        {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{this.title}");  
+            Console.WriteLine($"{this.author}");
+            Console.WriteLine($"{this.year}");
+        }
+    }
+}
